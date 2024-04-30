457911aad767d6c59f0707256e33880158637e64
Nathan Miller <nmiller217@gmail.com>
2024-04-30 15:20:37 -0700

    add extension class

---
+++ OdbDesignClientLib/Extensions/UriExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace Odb.Client.Lib.Extensions
+{
+    public static class UriExtensions
+    {
+        public static string GetQueryParameter(this Uri uri, string key)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            //var queryParams = HttpUtility.ParseQueryString(NavigationManager.ToAbsoluteUri(NavigationManager.Uri).Query);
+            //var returnUrl = queryParams[RETURN_URL_QUERY_NAME] ?? "";
+
+            return default;
+        }
+    }
+}
