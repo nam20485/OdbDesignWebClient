f01d45a1a659409213b0641c66abf6edfc1b4743
Nathan Miller <nmiller217@gmail.com>
2024-05-01 11:52:28 -0700

    wrap JS interop in a service

---
+++ OdbDesignClientLib/Interop/IJsInteropProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odb.Client.Lib.Interop
+{
+    public interface IJsInteropProvider
+    {
+        ValueTask<TReturn> InvokeAsync<TReturn>(string name, params object[] @params);
+        ValueTask InvokeVoidAsync(string name, params object[] @params);
+        TReturn Invoke<TReturn>(string name, params object[] @params);
+        void InvokeVoid(string name, params object[] @params);
+    }
+}
---
+++ OdbDesignClientLib/Interop/JsInteropProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.JSInterop;
+
+namespace Odb.Client.Lib.Interop
+{
+    public class JsInteropProvider : IJsInteropProvider
+    {
+        //private readonly IJSRuntime _jsRuntime;
+        private readonly IJSInProcessRuntime _jsInProcessRuntime;
+
+        public JsInteropProvider(IJSRuntime jsRuntime)
+        {
+            //_jsRuntime = jsRuntime;
+            _jsInProcessRuntime = jsRuntime as IJSInProcessRuntime;
+        }
+
+        public TReturn Invoke<TReturn>(string name, params object[] @params)
+        {
+            return _jsInProcessRuntime.Invoke<TReturn>(name, @params);
+        }
+
+        public void InvokeVoid(string name, params object[] @params)
+        {
+            _jsInProcessRuntime.InvokeVoid(name, @params);
+        }
+
+        public async ValueTask<TReturn> InvokeAsync<TReturn>(string name, params object[] @params)
+        {
+            return await _jsInProcessRuntime.InvokeAsync<TReturn>(name, @params);
+        } 
+        
+        public async ValueTask InvokeVoidAsync(string name, params object[] @params)
+        {
+            await _jsInProcessRuntime.InvokeVoidAsync(name, @params);
+        }
+    }
+}
