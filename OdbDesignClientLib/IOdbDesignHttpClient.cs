86849a1a54508197aef5b4dd9aad4d05d815da90
Nathan Miller <nmiller217@gmail.com>
2024-05-02 12:20:56 -0700

    add interface for OdbDesignHttpClient

---
+++ OdbDesignClientLib/IOdbDesignHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.Forms;
+
+using Odb.Client.Lib.Model;
+
+namespace Odb.Client.Lib
+{
+    public interface IOdbDesignHttpClient
+    {
+        Design FetchDesign(string name);
+        Task<Design> FetchDesignAsync(string name);
+        FileArchive FetchFileArchive(string name);
+        Task<FileArchive> FetchFileArchiveAsync(string name);
+        FileArchiveListResponse FetchFileArchiveList();
+        Task<FileArchiveListResponse> FetchFileArchiveListAsync();
+        Task<FileUploadResponse> UploadDesignFileAsync(OdbDesignHttpClient.DesignFileUploadInfo uploadFileInfo);
+        Task<FileUploadResponse> UploadDesignFilesAsync(IEnumerable<OdbDesignHttpClient.DesignFileUploadInfo> uploadFileInfos);
+        Task<FileUploadResponse> UploadDesignFilesAsync(IEnumerable<IBrowserFile> browserFiles);
+    }
+}
