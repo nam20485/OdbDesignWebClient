e8d9070896bd38c931cbbf2428c1428c0727b047
Nathan Miller <nmiller217@gmail.com>
2024-05-02 12:21:09 -0700

    add Constgnats class

---
+++ OdbDesignClientLib/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odb.Client.Lib
+{
+    public static class Constants
+    {
+        public static class Strings
+        {
+
+        }
+
+        public static class Uris
+        {
+
+        }
+
+        public static class Numbers
+        {
+            public const int BYTES_PER_MEGABYTE = 1024 * 1024;
+        }
+    }
+}
