37a98f6f25340c3446468de603464d571ae2352f
Nathan Miller <nmiller217@gmail.com>
2024-05-11 14:23:58 -0700

    add AuthUtils

---
+++ Utils/AuthUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Utils
+{
+    public static class AuthUtils
+    {
+        private const string Scheme = "Basic";
+
+        public static AuthenticationHeaderValue MakeAuthHeaderValue(string username, string password)
+        {
+            if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
+            {
+                var authHeaderValue = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
+                //var authHeaderValue = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes($"{username}:{password}"));
+                return new AuthenticationHeaderValue(Scheme, authHeaderValue);
+            }
+
+            return null;
+        }      
+    }
+}
