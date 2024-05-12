26afc40615f7375faa526a7c09385eafe1942f7a
Nathan Miller <nmiller217@gmail.com>
2024-05-12 12:51:15 -0700

    add ExitCode enum

---
+++ Utils/ExitCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    public enum ExitCode
+    {
+        Success = 0,
+        UnknownError = 1,
+        InvalidArguments = 2,
+    }
+}
